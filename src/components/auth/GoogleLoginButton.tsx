ee784f094d086812b36649f3b1e0de586fa26bce
LEtMeDEv <79841@naver.com>
2024-01-25 22:24:09 +0900

    feat: redirect to home

--- src/components/auth/GoogleLoginButton.tsx
+++ src/components/auth/GoogleLoginButton.tsx
@@ -3,15 +3,18 @@ import GoogleLogin, {
   GoogleLoginResponseOffline,
 } from "react-google-login";
 import { useGoogleAccessTokenStore } from "../../lib/zustand";
+import { useNavigate } from "react-router-dom";
 
 export const GoogleLoginButton = () => {
   const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID as string;
   const { setAccessToken } = useGoogleAccessTokenStore();
 
+  const navigate = useNavigate();
+
   const onSuccess = (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
     if ("accessToken" in res) {
       setAccessToken(res.accessToken);
-      console.log(res.accessToken);
+      navigate("/");
     }
   };
 
