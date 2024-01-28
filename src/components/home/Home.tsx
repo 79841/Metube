998c172583a1026abd6e6e82d6457acfec6e011e
LEtMeDEv <79841@naver.com>
2024-01-28 23:33:29 +0900

    feat: update dnd

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -16,36 +16,36 @@ export const Home = () => {
   }, [data, setSubscribedList]);
 
   return (
-    <DndContextProvider>
-      <DndSpace>
-        <div className="h-full w-full">
-          <div className="flex h-fit justify-center px-8 pt-8">
-            <div className="mr-auto text-2xl font-semibold text-zinc-100">
-              Metube
-            </div>
-            <input
-              className="h-8 w-80 rounded-lg bg-zinc-800 p-4 text-zinc-100 outline-none backdrop-blur-md"
-              type="text"
-              placeholder="Search..."
-            />
-            <div className="ml-auto text-2xl">Metube</div>
-          </div>
-          <div className="flex h-fit w-full gap-8 p-8">
-            {/* <GoogleLogoutbutton /> */}
-            {!isLoading && (
-              <>
-                <div className="sticky top-16 h-fit">
-                  <SubscriptionList />
-                </div>
-                <div className="flex-[1]">
-                  <VideoList />
-                </div>
-              </>
-            )}
-          </div>
+    <div className="h-full w-full">
+      <div className="flex h-fit justify-center px-8 pt-8">
+        <div className="mr-auto text-2xl font-semibold text-zinc-100">
+          Metube
         </div>
-        <VideoModal />
-      </DndSpace>
-    </DndContextProvider>
+        <input
+          className="h-8 w-80 rounded-lg bg-zinc-800 p-4 text-zinc-100 outline-none backdrop-blur-md"
+          type="text"
+          placeholder="Search..."
+        />
+        <div className="ml-auto text-2xl">Metube</div>
+      </div>
+      <div className="flex h-fit w-full gap-8 p-8">
+        {/* <GoogleLogoutbutton /> */}
+        {!isLoading && (
+          <>
+            <div className="sticky top-16 h-fit">
+              <SubscriptionList />
+            </div>
+            <div className="flex-[1]">
+              <VideoList />
+            </div>
+          </>
+        )}
+      </div>
+      <DndContextProvider>
+        <DndSpace>
+          <VideoModal />
+        </DndSpace>
+      </DndContextProvider>
+    </div>
   );
 };
