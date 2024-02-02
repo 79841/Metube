27b10c9f4178b325e1fcf3757fdeae63088a3d25
LEtMeDEv <79841@naver.com>
2024-02-02 21:53:52 +0900

    feat: use suspense

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 
 import { useSubscribeList } from "../../hooks/useSubscribeList";
 import { useSubscribedListStore } from "../../lib/zustand/subscribedList";
@@ -15,20 +15,18 @@ export const Home = () => {
   }, [data, setSubscribedList]);
 
   return (
-    <div className="min-h-screen w-full">
+    <div className="flex min-h-screen w-full flex-col">
       <Header />
       <div className="flex h-full w-full gap-4 p-8">
         {/* <GoogleLogoutbutton /> */}
-        {!isLoading && (
-          <>
-            <div className="sticky top-16 h-fit">
-              <SubscriptionList />
-            </div>
-            <div className="h-full flex-[1]">
-              <VideoList />
-            </div>
-          </>
-        )}
+        <div className="sticky top-16">
+          <SubscriptionList />
+        </div>
+        <div className="h-full flex-[1]">
+          <Suspense fallback={<div>loading...</div>}>
+            <VideoList />
+          </Suspense>
+        </div>
       </div>
       <div className="fixed bottom-0 right-0 h-fit w-fit">
         <VideoModal />
